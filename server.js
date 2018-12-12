2aa28ccb857c012b939b3bc8ea523834bb0dd5f7
mariliamessias <marilia.oliveira@outlook.com>
2018-12-11 23:04:51 -0200

    aaaa

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/authenticate', (req, res) => {
 })
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, err => {
   if (err) {
     console.error(err)
